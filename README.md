ef6fbaf9969df8faa029a4db0626951f6239c6ae
zonghan <xzonghanx@gmail.com>
2024-04-25 22:08:50 +0800

    amend loading gif source as preloadlink

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Whats for Dinner?!!?!?!!?!!?!
 ## Links
 
 UserStory: https://trello.com/invite/b/YTyXgWkq/ATTIb3c653ec5532a6e86f2b4c56c3fe53ac726E9FDA/ga-project2
-Vercel: https://cookbook-nom5q8zp1-zonghans-projects-5edc3094.vercel.app
+Vercel: https://cookbook-ten-ochre.vercel.app
--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,11 +1,12 @@
 const Loading = () => {
-	const handleImageError = (error) => {
-		console.error("here", error);
-	};
+	// const handleImageError = (error) => {
+	// 	console.error("here", error);
+	// };
 
 	return (
 		<div className='loading'>
-			<img src='../ricefrying.webp' alt='loading...' onError={(error) => handleImageError(error)} />
+			{/* <img src='../ricefrying.webp' alt='loading...' onError={(error) => handleImageError(error)} /> */}
+			<img src='https://64.media.tumblr.com/a3c29f302a84dde45cf07639711f4018/tumblr_ot69mcbeYC1uxvvvzo3_500.gifv' />
 		</div>
 	);
 };
